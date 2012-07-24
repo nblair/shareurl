db121ed0cc99688e320bfe6d02a807a429d11912
npblair <npblair@3f4f5be6-2691-46ed-a3ac-a51c1323bd7b>
2012-07-24 14:10:57 +0000

    CALKEY-108 remove unused methods

--- src/main/java/edu/wisc/wisccal/shareurl/web/GuessableShareManagementController.java
+++ src/main/java/edu/wisc/wisccal/shareurl/web/GuessableShareManagementController.java
@@ -1,20 +1,5 @@
 /**
- * Licensed to Jasig under one or more contributor license
- * agreements. See the NOTICE file distributed with this work
- * for additional information regarding copyright ownership.
- * Jasig licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a
- * copy of the License at:
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
  */
 
 package edu.wisc.wisccal.shareurl.web;
@@ -59,19 +44,6 @@ public class GuessableShareManagementController {
 		this.shareDao = shareDao;
 	}
 	
-	@RequestMapping(value="/my-public",method=RequestMethod.GET )
-	public String getMyGuessable(ModelMap model) {
-		CalendarAccountUserDetails currentUser = (CalendarAccountUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		ICalendarAccount activeAccount = currentUser.getCalendarAccount();
-		if(log.isDebugEnabled()) {
-			log.debug("handling getMyGuessable request for " + activeAccount);
-		}
-		
-		Share share = shareDao.retrieveGuessableShare(activeAccount);
-		model.put("share", share);
-		return "jsonView";
-	}
-	
 	@RequestMapping(value="/create-public",method=RequestMethod.POST )
 	public String createDefaultGuessable(ModelMap model) {
 		CalendarAccountUserDetails currentUser = (CalendarAccountUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -93,19 +65,4 @@ public class GuessableShareManagementController {
 		return "jsonView";
 	}
 	
-	@RequestMapping(value="/remove-public", method=RequestMethod.POST)
-	public String removeGuessable(ModelMap model) {
-		CalendarAccountUserDetails currentUser = (CalendarAccountUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		ICalendarAccount activeAccount = currentUser.getCalendarAccount();
-		if(log.isDebugEnabled()) {
-			log.debug("handling removeGuessable request for " + activeAccount);
-		}
-		
-		Share share = shareDao.retrieveGuessableShare(activeAccount);
-		if(share != null) {
-			shareDao.revokeShare(share);
-		}
-		model.addAttribute("success", true);
-		return "jsonView";
-	}
 }
